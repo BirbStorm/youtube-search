4abea8a53ec3f185a71f8c1039522a3eca3e1047
Andy <russellandy98@gmail.com>
2019-08-28 17:22:33 -0400

    imported YTSearch and added videos state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SearchBar from "./components/search_bar";
-import * as serviceWorker from './serviceWorker';
+import YTSearch from 'youtube-api-search';
+// import * as serviceWorker from './serviceWorker';
 
 const API_KEY = 'AIzaSyAsvLVh8NrTX0tWZ2sR5OQ0Exh0soFttk4'
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+            // ES6 syntax for this.setState({ videos: videos});
+            this.setState({ videos })
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -19,4 +32,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
