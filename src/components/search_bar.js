c8599c32fffcbd37ba1670dcb55d49b4dfbd0a5c
Andy <russellandy98@gmail.com>
2019-09-03 17:30:56 -0400

    fixed search bar issue I was having

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,10 +8,12 @@ class SearchBar extends Component {
     }
     render(){
         return (
-            <div className="search-bar"><input
+            <div className="search-bar">
+                <input
                 value={this.state.term}
                 onChange= {event => this.setState({ term: event.target.value })}
-            /></div>)
+                />
+            </div>)
     }
 }
 
